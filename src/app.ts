a49e2bdeb0f2f571158612942ed232f8a04e66ea
coder-sadhin <codersadhin@gmail.com>
2023-11-19 22:06:03 +0600

    initial files

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({
+    Operation: 'Success',
+  });
+});
+
+export default app;
